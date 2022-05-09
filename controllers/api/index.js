e8bde34e70b361f477e8d7ef6f953259634bcf84
megan <mtpott23@gmail.com>
2022-05-09 19:57:56 -0400

    work through sell coin functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes.js");
 const walletRoutes = require("./wallet-routes");
+const sellRoutes = reqiure("./sell-routes");
 
 router.use("/user", userRoutes);
 router.use("/wallet", walletRoutes);
+router.use("/sell", sellRoutes);
 
 module.exports = router;
