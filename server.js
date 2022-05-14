7114f28b5f0c061767ae8c98d71fe565c82a316d
ApolloSolo <matt1teixeira@gmail.com>
2022-05-14 14:46:41 -0400

    Reset DB

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ app.use(require("./controllers/"));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: true });
+  sequelize.sync({ force: false });
 });
